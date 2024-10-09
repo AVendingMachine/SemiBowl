dda8cee952d9837ad1b5d06aa991faf04ab89f57
AVendingMachine <79037156+AVendingMachine@users.noreply.github.com>
2024-10-08 20:04:07 -0400

    Working on a startpage

---
+++ SemiBowl-App/src/components/NavBar.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { RouterLink} from 'vue-router'
+</script>
+<template>
+    <div class="navbar">
+        <RouterLink to="/">Semibowl</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+    </div>
+</template>
--- SemiBowl-App/src/views/HomeView.vue
+++ SemiBowl-App/src/views/HomeView.vue
@@ -1,9 +1,3 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>this is the home</h1>
 </template>
