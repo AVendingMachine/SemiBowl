897791db2100fb694e8f44b66627c80b0cae189c
AVendingMachine <79037156+AVendingMachine@users.noreply.github.com>
2024-10-19 20:02:57 -0400

    Update AuthorsQuiz.vue

--- SemiBowl-App/src/components/AuthorsQuiz.vue
+++ SemiBowl-App/src/components/AuthorsQuiz.vue
@@ -2,12 +2,13 @@
 import { ref } from 'vue'
 import { questions } from '@/assets/questions';
 const clues = questions //Object array which stores all of the author questions
-const showQuiz = ref("false")
+const showQuiz = ref(false)
 const regularQuestionOrder = ref([0])
 const randomQuestionOrder = ref([0])
 const rightWrong = ref("bunga")
 const currentInput = ref("")
 const winMessage = ref("")
+const difficulty = ref(0)
 let questionOrder = [0]
 showQuiz.value = false
 randomQuestionOrder.value = [0]
@@ -18,7 +19,7 @@ function randomInt(min,max) {
     return Math.floor(Math.random()*(max-min +1)) + min
 }
 function randomizeArray(orderedArray) {
-    let randomizedArray = [0]
+    let randomizedArray = [0]++
     let copiedArray = orderedArray.slice()
     let n = 0
     while (copiedArray.length > 0) {
@@ -32,29 +33,48 @@ function randomizeArray(orderedArray) {
 async function startQuiz() {
     showQuiz.value=true
     for (let i = 0;i<clues.length;i++) {
-        questionOrder[i] = i;
+        questionOrder[i] = i;+
     }
     regularQuestionOrder.value = questionOrder
     randomQuestionOrder.value = randomizeArray(questionOrder)
 }
+function checkIfCorrect(answer) {
+  currentInput.value = ""
+  if (answer.toLowerCase() === clues[randomQuestionOrder.value[0]].author.toLowerCase()) {
+    regularQuestionOrder.value.shift()
+    randomQuestionOrder.value.shift()
+    return true
+  }
+  else if (randomQuestionOrder.value.length === 0) {
+    difficulty.value++
+  }
+  else {
+    regularQuestionOrder.value.push(regularQuestionOrder.value[0])
+    regularQuestionOrder.value.shift()
+    randomQuestionOrder.value.push(randomQuestionOrder.value[0])
+    randomQuestionOrder.value.shift()
+    return false
+  }
+}
 function advanceQuestion(answer) {
-    if (answer.toLowerCase() == clues[randomQuestionOrder.value[0]].author.toLowerCase()) {
-        rightWrong.value = "right"
+    currentInput.value = ""
+    if (answer.toLowerCase() === clues[randomQuestionOrder.value[0]].author.toLowerCase()) {
         regularQuestionOrder.value.shift()
         randomQuestionOrder.value.shift()
+        return true
     }
-    else if (randomQuestionOrder.value.length == 0) {
-        showQuiz.value = false
+    else if (randomQuestionOrder.value.length === 0) {
+        difficulty.value++
     }
     else {
-        rightWrong.value = "wrong"
         regularQuestionOrder.value.push(regularQuestionOrder.value[0])
         regularQuestionOrder.value.shift()
         randomQuestionOrder.value.push(randomQuestionOrder.value[0])
         randomQuestionOrder.value.shift()
+        return false
     }
-    currentInput.value = ""
 }
+
 </script>
 <template>
     <button v-if="!showQuiz" @click="startQuiz">Start</button>
@@ -63,7 +83,7 @@ function advanceQuestion(answer) {
         <p>{{ clues[randomQuestionOrder[0]].easyWork }}</p>
         <p>{{ winMessage }}</p>
         <div v-if="showQuiz">
-            <form  @submit.prevent="advanceQuestion(currentInput)">
+            <form  @submit.prevent="checkIfCorrect(currentInput)">
                 <input type="text" v-model.trim="currentInput"><br><br>
                 <button>Submit</button>
             </form>
